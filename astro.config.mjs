768a9f8733f4da9537566094937d3d68c4675fae
kjacks <kirkgjackson@gmail.com>
2025-02-18 13:23:27 +0100

    Deleted Astro boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [
+            tailwindcss()
+        ],
     },
 });
