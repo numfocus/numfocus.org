db052728fd09f86f3d9dddfe35abc641602d6454
kjacks <kirkgjackson@gmail.com>
2025-02-21 14:34:42 +0100

    Impolemented filter logic in ProjectGrid

--- src/components/ProjectGrid/ProjectGrid.tsx
+++ src/components/ProjectGrid/ProjectGrid.tsx
@@ -1,28 +1,37 @@
 import { useMemo, useState } from 'react';
-import type { Project } from 'env';
+import type { Project, ProjectFilterOptions, ProjectFilterValues } from 'env';
 import ProjectCard from './ProjectCard';
 import ProjectFilters from './ProjectFilters';
 
-export default function ProjectGrid({ filterValues, projects }: { filterValues: any, projects: Project[] }) {
-  const [activeFilters, setActiveFilters] = useState({});
+export default function ProjectGrid({ filterOptions, projects }: { filterOptions: ProjectFilterOptions, projects: Project[] }) {
+  const [activeFilters, setActiveFilters] = useState<ProjectFilterValues>({ features: [], industries: [], languages: []});
 
-  // This will run each time `filters` or `projects` change.
   const filteredProjects = useMemo(
     () => {
+      console.log('in memo', activeFilters, projects)
       return projects.filter((project, i) => {
         return true;
-        // return i === 1
+        // const commonItems = activeFilters.industries.filter(item => project.industries.includes(item));
+        // return commonItems.length > 0
       });
     },
     [activeFilters, projects]
   );
 
+  const setActiveFilter = (key: string, items: any) => {
+    setActiveFilters({...activeFilters, [key]: items })
+  }
+
   return (
     <div>
-      <ProjectFilters filterValues={filterValues} activeFilters={activeFilters} setActiveFilters={setActiveFilters} />
+      <ProjectFilters
+        filterOptions={filterOptions}
+        activeFilters={activeFilters}
+        onChange={setActiveFilter}
+      />
       <div className="project-grid mb-64 grid grid-cols-4 gap-8">
         {filteredProjects.map((project) => {
-          return <ProjectCard project={project} />;
+          return <ProjectCard project={project} key={project.id} />;
         })}
       </div>
     </div>
