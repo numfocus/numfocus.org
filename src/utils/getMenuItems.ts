2bfa4db95f588bfc8d6699c26006ce820e5b6d38
kjacks <kirkgjackson@gmail.com>
2025-03-26 12:21:36 +0100

    Created getMenuItmes util function

---
+++ src/utils/getMenuItems.ts
@@ -0,0 +1,26 @@
+import directus from '../../lib/directus';
+import { readItems } from '@directus/sdk';
+
+import type { MenuItem } from 'env';
+
+export default async function getMenuItems(menuName: string) {
+  const menu = await directus.request(
+    readItems('Menus', {
+      filter: { menu_name: { _eq: menuName } },
+      fields: ['id', 'menu_name', 'item.item.*'],
+    })
+  );
+  const itemsFromMenu = menu[0].item;
+
+  const menuItems: MenuItem[] = [];
+
+  for (const item of itemsFromMenu) {
+    const singleItem: MenuItem = {
+      title: item.item.title,
+      path: `/${item.item.slug}`,
+    };
+    menuItems.push(singleItem);
+  }
+
+  return menuItems
+}
