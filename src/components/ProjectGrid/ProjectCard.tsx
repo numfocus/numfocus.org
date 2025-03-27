3aec0b39a08ec1aee1beabc2ccd5298072e45f89
kjacks <kirkgjackson@gmail.com>
2025-03-27 17:09:31 +0100

    Added mobile formatting for ProjectCard

--- src/components/ProjectGrid/ProjectCard.tsx
+++ src/components/ProjectGrid/ProjectCard.tsx
@@ -2,7 +2,10 @@ import type { Project } from 'env';
 import { MoveDiagonal, Link2, Star } from 'lucide-react';
 import { twMerge } from 'tailwind-merge';
 import Markdown from 'react-markdown';
-const footerDefaultStyle = `flex absolute bottom-0 h-12 w-full place-items-center border justify-center gap-2`;
+
+interface StyledComponentProps { className?: string }
+
+const footerDefaultStyle = `flex relative bottom-0 h-12 w-full place-items-center border justify-center gap-2`;
 
 export default function ProjectCard({
   project,
@@ -12,8 +15,21 @@ export default function ProjectCard({
   onExpand: () => void;
 }) {
   const { name, logo, short_description, type } = project.data;
+
+  const CardImage = ({ className }: StyledComponentProps) => (
+    <img src={logo.src} alt={name} className={twMerge("my-0", className)} />
+  )
+  const CardTitle = ({ className }: StyledComponentProps) => (
+    <h5 className={twMerge(className)}>{name}</h5>
+  )
+  const CardDescription = ({ className }: StyledComponentProps) => (
+    <div className={twMerge(className)}>
+      <Markdown>{short_description}</Markdown>
+    </div>
+  )
+
   return (
-    <div className="project-card border-numfocus-primary col-span-6 lg:col-span-4 border relative">
+    <div className="project-card border-numfocus-primary col-span-6 lg:col-span-4 xl:col-span-3 border relative">
       <div
         className="absolute top-0 right-0 flex cursor-pointer place-items-center gap-2 bg-teal-600 p-2 text-white hover:bg-teal-700"
         onClick={onExpand}
@@ -21,15 +37,23 @@ export default function ProjectCard({
         Expand
         <MoveDiagonal className="inline" />
       </div>
-        
-      <div className="overflow-hidden px-4 pt-12 pb-20 sm:px-6">
-        <h5 className="">{name}</h5>
-        <img src={logo.src} alt={name} className="mx-auto my-0 h-30" />
-        <div className="text-center text-sm">
-          <Markdown>{short_description}</Markdown>
+      
+      <div className="overflow-hidden flex flex-col h-full">
+        {/* Mobile */}
+        <div className="md:hidden overflow-hidden px-6 py-5 flex flex-row gap-6">
+          <CardImage className="h-20"/>
+          <div>
+            <CardTitle className="pb-3 text-md" />
+            <CardDescription />
+          </div>
         </div>
-        {/* <a href={website_link}>{website_link}</a> */}
-      </div>
+
+        {/* Desktop */}
+        <div className="hidden md:block overflow-hidden grow-1 px-4 pt-10 pb-6">
+          <CardTitle />
+          <CardImage className="mx-auto h-25" />
+          <CardDescription className="text-center" />
+        </div>
         {type === 'affiliated' && (
           <div
             className={twMerge(
@@ -52,6 +76,7 @@ export default function ProjectCard({
             Sponsored Project
           </div>
         )}
+      </div>
     </div>
   );
 }
