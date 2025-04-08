d5643195dc43cbf5040b15dc1c5dbcb41b41d936
kjacks <kirkgjackson@gmail.com>
2025-04-08 13:12:23 +0200

    Used BlockImage inside BLockHero component

--- src/components/TipTapRender/BlockHero.tsx
+++ src/components/TipTapRender/BlockHero.tsx
@@ -1,18 +1,17 @@
+import BlockImage from './BlockImage';
+
 interface Props {
   headline: string;
   subheader?: string;
   rich_text: string;
-  image?: string;
-  title?: string;
+  image?: any;
 }
-const assetBaseUrl = `${import.meta.env.DIRECTUS_URL}assets/`;
 
 export default function BlockHero({
   headline,
   subheader,
   rich_text,
   image,
-  title,
 }: Props) {
   return (
     <div className="bg-light-teal-50 grid grid-cols-12 pt-0 pl-0 md:py-8 md:pl-24">
@@ -26,11 +25,7 @@ export default function BlockHero({
         <div className="buttons mr-12 flex flex-row gap-2"></div>
       </div>
       <div className="order-first col-span-full mb-4 object-fill md:order-none md:col-span-5 md:col-start-8 md:mb-0">
-        <img
-          src={`${assetBaseUrl}${image}`}
-          alt={title}
-          className="w-full overflow-hidden rounded-none md:rounded-l-2xl md:rounded-r-none"
-        />
+        <BlockImage image={image} />
       </div>
     </div>
   );
