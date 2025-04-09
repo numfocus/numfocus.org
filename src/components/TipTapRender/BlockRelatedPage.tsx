a7117fc9a45994d900a8fa67f39d513deb8beaa0
kjacks <kirkgjackson@gmail.com>
2025-04-09 14:37:36 +0200

    Added variant styling to BlockRelatedPage

--- src/components/TipTapRender/BlockRelatedPage.tsx
+++ src/components/TipTapRender/BlockRelatedPage.tsx
@@ -1,5 +1,5 @@
-import PrettyJson from '@components/Atoms/PrettyJson';
 import Button from '@components/Atoms/Button';
+import { twMerge } from 'tailwind-merge';
 
 interface Props {
   page: any
@@ -8,27 +8,28 @@ interface Props {
 export default function BlockRelatedPage({
   page
 }: Props) {
-  const { button_text: buttonText, style } = page.block_related_page_id;
+  const { button_text: buttonText, variant } = page.block_related_page_id;
   const { slug, title } = page.item;
 
-  const heading = page.collection === 'case_studies' ? 'Case Study' : null
+  const heading = page.collection === 'case_studies' ? 'Case Study' : 'Lorem Ipsum'
   const linkTarget = `${page.collection === 'case_studies' ? '/case-studies' : ''}/${slug}`;
 
+  const blockStyle = variant === 'primary' ? 'bg-blue-400 text-white' : 'border-t-6 border-b-6 border-blue-400';
+
   return (
-    <div className="float-right">
+    <div className={twMerge("float-right sm:w-full md:w-1/2 lg:w-1/3 px-5 py-5 m-4", blockStyle)}>
       {heading && (
-        <p>{heading}</p>
+        <p className='italic'>{heading}</p>
       )}
-      <h5>{title}</h5>
+      <h5 className="py-4 font-normal">{title}</h5>
       <Button
         button={{
           text: buttonText,
           link: linkTarget,
-          variant: "primary"
+          variant: variant === 'primary' ? "primary" : "primaryBlue"
         }}
         arrow="right"
       />
-      {PrettyJson(page)}
     </div>
   );
 }
