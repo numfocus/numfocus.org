0a7b94cffb1dcfbf5cde01864e9d90281c9a1fd2
Tin Geber <tingeber@gmail.com>
2025-04-23 12:07:52 +0200

    SEO improvements

---
+++ src/utils/getPageSeo.ts
@@ -0,0 +1,19 @@
+import type { Seo } from 'env';
+import directus from '../../lib/directus';
+import { readItems, readSingleton } from '@directus/sdk';
+
+export default async function getPageSeo(slug: string) {
+  const general = await directus.request(readSingleton('General'));
+  const payload = await directus.request(readItems('pages'));
+  const content = payload.filter((e: any) => e.slug === slug)[0];
+  const pageSeo: Seo = {
+    title: content.seo?.title ? content.seo.title : '',
+    meta_description: content.seo?.meta_description
+      ? content.seo.meta_description
+      : '',
+    og_image: content.seo?.og_image
+      ? content.seo.og_image
+      : general.seo.og_image,
+  };
+  return pageSeo;
+}
