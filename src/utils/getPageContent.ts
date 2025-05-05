1df1abd9502a9b9aac5903d3f6425ca9f2d4cc38
kjacks <kirkgjackson@gmail.com>
2025-05-05 12:21:11 +0200

    Started [slug].astro implementation

--- src/utils/getPageContent.ts
+++ src/utils/getPageContent.ts
@@ -2,14 +2,14 @@ import { readItems } from '@directus/sdk';
 import { injectDataIntoContent } from 'directus-extension-flexible-editor/content';
 import directus from '../../lib/directus';
 
-export default async function getPageContent(slug: string) {
+export default async function getPageContent() {
   const result = await directus.request(
     readItems('pages', {
-      filter: {
-        "slug": {
-          "_eq": slug
-        }
-      },
+      // filter: {
+      //   "slug": {
+      //     "_eq": slug
+      //   }
+      // },
       fields: [
         'id',
         'title',
@@ -21,11 +21,22 @@ export default async function getPageContent(slug: string) {
       ],
     })
   );
-  const page = result?.[0];
 
-  if (page) {
-    return injectDataIntoContent(page.editor_nodes, page.body_content)
+  console.log(result)
+
+  if (result) {
+    return result.map(page => {
+      if (page.body_content) {
+        return ({
+          ...page,
+          body_content: injectDataIntoContent(page.editor_nodes, page.body_content)
+        })
+      } else {
+        return page
+      }
+      })
   } else {
+    return null
     // TODO: meaningfully handle missing page content error?
   }
 }
