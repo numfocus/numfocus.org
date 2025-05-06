fc7825ee4c23442578639ba7d9ea888ab38099e6
kjacks <kirkgjackson@gmail.com>
2025-05-06 16:29:22 +0200

    Reverted getPageContent util function

--- src/utils/getPageContent.ts
+++ src/utils/getPageContent.ts
@@ -2,14 +2,14 @@ import { readItems } from '@directus/sdk';
 import { injectDataIntoContent } from 'directus-extension-flexible-editor/content';
 import directus from '../../lib/directus';
 
-export default async function getPageContent() {
+export default async function getPageContent(slug: string) {
   const result = await directus.request(
     readItems('pages', {
-      // filter: {
-      //   "slug": {
-      //     "_eq": slug
-      //   }
-      // },
+      filter: {
+        slug: {
+          _eq: slug,
+        },
+      },
       fields: [
         'id',
         'title',
@@ -21,22 +21,9 @@ export default async function getPageContent() {
       ],
     })
   );
+  const page = result?.[0];
 
-  console.log(result)
-
-  if (result) {
-    return result.map(page => {
-      if (page.body_content) {
-        return ({
-          ...page,
-          body_content: injectDataIntoContent(page.editor_nodes, page.body_content)
-        })
-      } else {
-        return page
-      }
-      })
-  } else {
-    return null
-    // TODO: meaningfully handle missing page content error?
+  if (page) {
+    return injectDataIntoContent(page.editor_nodes, page.body_content);
   }
 }
