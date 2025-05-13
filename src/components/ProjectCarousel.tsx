88f6d83ee26daf8b8cfca74c73e45a8c322d1acc
kjacks <kirkgjackson@gmail.com>
2025-05-13 11:26:53 +0200

    Addedd Project Dialog to homepage project carousel

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -5,29 +5,40 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@lib/components/ui/carousel';
+import { useState } from 'react';
 
+import Dialog from '@components/Atoms/Dialog';
+import ProjectDialogContent from '@components/ProjectGrid/ProjectExpandedContent';
 import type { Project } from 'env';
 import ProjectCard from './ProjectGrid/ProjectCard';
 
 export default function ProjectCarousel({ projects }: { projects: Project[] }) {
+  const [expandedProject, setExpandedProject] = useState<Project | undefined>();
+  
   return (
-    <Carousel opts={{ align: 'start' }}>
-      <CarouselContent className="-ml-8">
-        {projects.map((project, i) => (
-          <CarouselItem
-            key={project.id}
-            className="basis-full self-stretch pl-8 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
-          >
-            <ProjectCard
-              project={project}
+    <>
+      <Carousel opts={{ align: 'start' }}>
+        <CarouselContent className="-ml-8">
+          {projects.map((project, i) => (
+            <CarouselItem
               key={project.id}
-              showMobileVersion={false}
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className="bg-brand-primary hover:bg-brand-primary cursor-pointer rounded-sm text-white transition hover:-translate-x-0.5 hover:text-white disabled:hidden" />
-      <CarouselNext className="bg-brand-primary hover:bg-brand-primary cursor-pointer rounded-sm text-white transition hover:translate-x-0.5 hover:text-white disabled:hidden" />
-    </Carousel>
+              className="basis-full self-stretch pl-8 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+            >
+              <ProjectCard
+                project={project}
+                key={project.id}
+                showMobileVersion={false}
+                onExpand={() => setExpandedProject(project)}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="bg-brand-primary hover:bg-brand-primary cursor-pointer rounded-sm text-white transition hover:-translate-x-0.5 hover:text-white disabled:hidden" />
+        <CarouselNext className="bg-brand-primary hover:bg-brand-primary cursor-pointer rounded-sm text-white transition hover:translate-x-0.5 hover:text-white disabled:hidden" />
+      </Carousel>
+      <Dialog open={!!expandedProject} onClose={() => setExpandedProject(undefined)}>
+        {expandedProject && <ProjectDialogContent project={expandedProject} />}
+      </Dialog>
+    </>
   );
 }
