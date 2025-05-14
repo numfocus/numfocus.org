b8c012082b4750de61e24bf8c7701e1f6a9645af
kjacks <kirkgjackson@gmail.com>
2025-05-14 14:58:13 +0200

    Modified Menu fetching to fetch menus by type

---
+++ src/utils/getMenusByType.ts
@@ -0,0 +1,22 @@
+import directus from '@directus/directus';
+import { readItems } from '@directus/sdk';
+
+import type { Menu } from 'env';
+
+export default async function getMenusByType(type: string) {
+  const menus = await directus.request(
+    readItems('Menus', {
+      filter: { type: { _eq: type } },
+      fields: ['id', 'title', 'item.item.*'],
+    })
+  );
+
+  return menus.map(({ id, title, item}) => ({
+    id,
+    title,
+    items: item.map((item: any) => ({
+      title: item.item.title,
+      path: `/${item.item.slug}/`,
+    }))
+  }))
+}
