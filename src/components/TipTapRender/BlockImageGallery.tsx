e1746cf8d0f3fffa3a1b42690b30c1663194eb4a
Tin Geber <tingeber@gmail.com>
2025-05-14 12:51:37 +0200

    first masonry draft

--- src/components/TipTapRender/BlockImageGallery.tsx
+++ src/components/TipTapRender/BlockImageGallery.tsx
@@ -6,19 +6,24 @@ interface Props {
 import getAssetUrl from '@utils/getAssetUrl';
 
 export default function BlockImageGallery({ images }: Props) {
-
-  console.log(images)
+  console.log(images);
   return (
-    <div>
+    <div className="mx-auto my-8 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5">
       {images.map(({ id, description, title }) => (
-        <div key={id}>
-           <img
-             src={getAssetUrl(id)}
-             alt={title}
-             className="overflow-hidden rounded-none md:rounded-l-2xl md:rounded-r-none"
-           />
-           {!!description && <p className="px-4 py-2 text-center">{description}</p>}
-         </div>
+        <div key={id} className="group relative transition">
+          <img
+            src={getAssetUrl(id)}
+            alt={title}
+            className="mb-4 rounded-sm border border-transparent transition hover:border-gray-300 hover:shadow-lg"
+          />
+          {!!description && (
+            <div className="inset absolute bottom-0 z-10 flex items-center justify-center">
+              <p className="bg-black/30 px-4 py-2 text-center font-sans text-xs text-white opacity-0 transition group-hover:opacity-100">
+                {description}
+              </p>
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
