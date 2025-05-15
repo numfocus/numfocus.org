6221439610925f62e6cac19c31806abb584df31a
kjacks <kirkgjackson@gmail.com>
2025-05-15 15:31:50 +0200

    fetchRemoteImages -> fetchRemoteImage

--- src/utils/injectFlexibleEditorContent.ts
+++ src/utils/injectFlexibleEditorContent.ts
@@ -1,22 +1,10 @@
 import { getImage } from 'astro:assets';
 import getAssetUrl from '@utils/getAssetUrl';
 import { injectDataIntoContent } from 'directus-extension-flexible-editor/content';
-import type { Image } from 'env';
+import type { CustomContentItem, Image } from 'env';
+import fetchRemoteImage from './fetchRemoteImage';
 
-const fetchImage = async (img) => {
-  const remoteUrl = getAssetUrl(img.id);
-  return new Promise<Image>((resolve) => {
-    getImage({
-      src: remoteUrl,
-      inferSize: true
-    }).then((fetchedImage) =>
-      resolve({
-        ...img,
-        src: fetchedImage?.src || remoteUrl
-      })
-    );
-  });
-}
+
 
 const fetchNodeImages = async (editorNode: any) => {
   const { collection, item } = editorNode;
@@ -24,31 +12,36 @@ const fetchNodeImages = async (editorNode: any) => {
   const newItem = item;
 
   if (collection === 'block_custom_content_group') {
-    const itemsWImages = item.items.map(({ block_custom_content_item_id }) => {
+    const itemsWImages = item.items.map(({ block_custom_content_item_id }: { block_custom_content_item_id: CustomContentItem }) => {
       return new Promise(resolve => {
-        fetchImage(block_custom_content_item_id.image).then(fetchedImage => {
+        fetchRemoteImage(block_custom_content_item_id.image).then(fetchedImage => {
           resolve({ ...block_custom_content_item_id, image: fetchedImage });
         })
       })
     })
-    
+
     newItem.items = await Promise.all(itemsWImages)
+
   } else if (collection === 'block_hero') {
-    newItem.image = await fetchImage(item.image)
+    newItem.image = await fetchRemoteImage(item.image)
+
   } else if (collection === 'block_image') {
-    newItem.image = await fetchImage(item.image)
+    newItem.image = await fetchRemoteImage(item.image)
+
   } else if (collection === 'block_image_gallery') {
     const fetchedImages = item.images.map(
-      ({ directus_files_id }: { directus_files_id: Image }) => fetchImage(directus_files_id)
+      ({ directus_files_id }: { directus_files_id: Image }) => fetchRemoteImage(directus_files_id)
     )
     newItem.images = await Promise.all(fetchedImages);
+
   } else if (collection === 'block_testimonial') {
-    newItem.image = await fetchImage(item.image)
+    newItem.image = await fetchRemoteImage(item.image)
+    
   } else if (collection === 'block_toc') {
     const imagePromises = item.editor_nodes?.map(fetchNodeImages)
 
     newItem.editor_nodes = await Promise.all(imagePromises);
-  }
+  } 
 
   return ({...editorNode, item: newItem})
 }
