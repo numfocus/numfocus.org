d8f32129eb5b7c8f5adfb67ef30a94cc3bdca06c
Tin Geber <tingeber@gmail.com>
2025-09-26 15:36:21 +0200

    re-adding trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
   adapter: netlify(),
   image: {
     domains: ['cms.numfocus.org'],
+    trailingSlash: 'always',
     // experimentalLayout: 'constrained',
   },
   integrations: [
